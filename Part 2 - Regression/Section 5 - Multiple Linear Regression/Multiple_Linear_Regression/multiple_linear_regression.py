657726ad40268db8d69f9538af175d7044fb24a7
Kirill Krolik <2902785+avant1@users.noreply.github.com>
2018-03-07 16:28:40 +0300

    multiple linear regression (python)

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -1,4 +1,4 @@
-# Multiple Linear Regression
+# Data Preprocessing Template
 
 # Importing the libraries
 import numpy as np
@@ -6,36 +6,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 4].values
+dataset = pd.read_csv('50_Startups.csv').as_matrix()
+X = dataset[:, :-1]
+y = dataset[:, -1]
 
-# Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder = LabelEncoder()
-X[:, 3] = labelencoder.fit_transform(X[:, 3])
-onehotencoder = OneHotEncoder(categorical_features = [3])
-X = onehotencoder.fit_transform(X).toarray()
+X[:, 3] = LabelEncoder().fit_transform(X[:, 3])
+X = OneHotEncoder(categorical_features=[3]).fit_transform(X).toarray()
 
-# Avoiding the Dummy Variable Trap
+# removing one dummy variable
+# cause it's presence does not bring any new information
+# however LinearRegression will perform redundancy check automatically
+# and remove one of those columns
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
-
-# Fitting Multiple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+score = regressor.score(X_test, y_test)
+print('Score is: {:.3f}'.format(score))
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
+predicted = regressor.predict(X_test)
+
+diff = predicted - y_test
+plt.hist(diff.reshape(-1, 1))
+plt.title('Errors of predictions for test set')
+plt.show()
