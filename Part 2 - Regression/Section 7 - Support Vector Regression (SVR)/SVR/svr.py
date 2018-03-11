6af4b3be482c01471cc5891e2e6d1bbd0c84abb1
Kirill Krolik <2902785+avant1@users.noreply.github.com>
2018-03-11 08:48:36 +0300

    SVR using python

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR/svr.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR/svr.py
@@ -1,4 +1,4 @@
-# SVR
+# Regression Template
 
 # Importing the libraries
 import numpy as np
@@ -6,43 +6,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+dataset = pd.read_csv('Position_Salaries.csv').as_matrix()
+X = dataset[:, 1:-1]
+y = dataset[:, -1]
 
 # Splitting the dataset into the Training set and Test set
-"""from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
+X_scaled = sc_X.fit_transform(X.astype(float))
 sc_y = StandardScaler()
-X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
+y_scaled = sc_y.fit_transform(y.astype(float).reshape(-1, 1))
 
-# Fitting SVR to the dataset
+# Fitting the Model to the dataset
 from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf')
-regressor.fit(X, y)
+regressor = SVR(kernel='rbf', C=3)
+regressor.fit(X_scaled, y_scaled)
 
-# Predicting a new result
-y_pred = regressor.predict(6.5)
-y_pred = sc_y.inverse_transform(y_pred)
+y_pred_scaled = regressor.predict(sc_X.transform(6.5))
+y_pred = sc_y.inverse_transform(y_pred_scaled)
+print(y_pred)
 
 # Visualising the SVR results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the SVR results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.scatter(X, y, color='red')
+predicted_y = sc_y.inverse_transform(regressor.predict(X_scaled))
+plt.plot(X, predicted_y, color='blue')
 plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
