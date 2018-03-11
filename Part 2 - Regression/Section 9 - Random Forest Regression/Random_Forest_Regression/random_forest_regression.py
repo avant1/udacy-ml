d923ecfbbaaf8d8d9d113891714de5d932aa3ccb
Kirill Krolik <2902785+avant1@users.noreply.github.com>
2018-03-11 21:55:08 +0300

    random forest (python)

--- Part 2 - Regression/Section 9 - Random Forest Regression/Random_Forest_Regression/random_forest_regression.py
+++ Part 2 - Regression/Section 9 - Random Forest Regression/Random_Forest_Regression/random_forest_regression.py
@@ -1,4 +1,4 @@
-# Random Forest Regression
+# Regression Template
 
 # Importing the libraries
 import numpy as np
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+dataset = pd.read_csv('Position_Salaries.csv').as_matrix()
+X = dataset[:, 1:-1]
+y = dataset[:, -1]
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.model_selection import train_test_split
@@ -22,20 +22,32 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Random Forest Regression to the dataset
-from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-regressor.fit(X, y)
 
-# Predicting a new result
-y_pred = regressor.predict(6.5)
+n_trees_to_try = [10, 100, 300, 3000, 10000]
 
-# Visualising the Random Forest Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
+plt.scatter(X, y, color='red', label='Input dataset')
+plt.title('Truth or Bluff (Random forest regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
+
+
+# Fitting the Regression Model to the dataset
+from sklearn.ensemble import RandomForestRegressor
+
+for n_trees in n_trees_to_try:
+    regressor = RandomForestRegressor(n_estimators=n_trees, random_state=0, n_jobs=-1)
+    regressor.fit(X, y)
+
+    # Predicting a new result
+    new_employee_level = 6.5
+    y_pred = regressor.predict(new_employee_level)[0]
+    print('For {} trees predicted salary is: {:.2f}'.format(n_trees, y_pred))
+
+    # Visualising the Regression results (for higher resolution and smoother curve)
+    X_grid = np.arange(min(X), max(X), 0.01)
+    X_grid = X_grid.reshape((len(X_grid), 1))
+    plt.plot(X_grid, regressor.predict(X_grid), label='Predicted values (n_trees={})'.format(n_trees))
+    plt.scatter(new_employee_level, y_pred, marker='x', label='Test value (n_trees={})'.format(n_trees))
+
+plt.legend()
 plt.show()
--- Part 2 - Regression/Section 9 - Random Forest Regression/Random_Forest_Regression/regression_template.py
+++ Part 2 - Regression/Section 9 - Random Forest Regression/Random_Forest_Regression/regression_template.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+dataset = pd.read_csv('Position_Salaries.csv').as_matrix()
+X = dataset[:, 1:-1]
+y = dataset[:, -1]
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.model_selection import train_test_split
@@ -29,8 +29,8 @@ y_train = sc_y.fit_transform(y_train)"""
 y_pred = regressor.predict(6.5)
 
 # Visualising the Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
+plt.scatter(X, y, color='red')
+plt.plot(X, regressor.predict(X), color='blue')
 plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
@@ -39,8 +39,8 @@ plt.show()
 # Visualising the Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
 plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
