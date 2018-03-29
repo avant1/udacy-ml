352c9a050973390dcb7bc13c19107f7ce6a40294
Kirill Krolik <2902785+avant1@users.noreply.github.com>
2018-03-29 09:54:09 +0300

    simplify clusters visualization

--- Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
@@ -24,17 +24,23 @@ X = dataset[:, -2:].astype(int)
 optimal_number_of_clusters = 5
 model = KMeans(n_clusters=optimal_number_of_clusters, random_state=0)
 model.fit(X)
-clusters = model.predict(X).reshape(-1, 1)
-cluster_numbers_with_initial_data = np.append(dataset, clusters, axis=1)
+clusters = model.predict(X)
 
 # agrrh, use get_named_colors_mapping instead
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 
-for row in cluster_numbers_with_initial_data:
-    cluster_number = row[-1]
+for cluster_number in range(0, optimal_number_of_clusters):
     color = colors[cluster_number]
-    year_income = row[-3]
-    spending_score = row[-2]
-    plt.scatter(year_income, spending_score, color=color)
+    plt.scatter(
+        X[clusters == cluster_number, 0],
+        X[clusters == cluster_number, 1],
+        s=50,
+        color=color,
+        label='Cluster {}'.format(cluster_number)
+    )
 
+plt.scatter(model.cluster_centers_[:, 0], model.cluster_centers_[:, 1], s=150, color='yellow', label='Centroids')
+plt.xlabel('Annual income (k$)')
+plt.ylabel('Spending score')
+plt.legend()
 plt.show()
