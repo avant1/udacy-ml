636fa2e43e9727d14caae6021e3942b6bcb788a1
Kirill Krolik <2902785+avant1@users.noreply.github.com>
2018-03-29 09:40:08 +0300

    kmeans using python

--- Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
@@ -1,53 +1,40 @@
-# K-Means Clustering
-
-# Importing the libraries
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Mall_Customers.csv')
-X = dataset.iloc[:, [3, 4]].values
-# y = dataset.iloc[:, 3].values
-
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
-
-# Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
-wcss = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
-    kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
-plt.plot(range(1, 11), wcss)
-plt.title('The Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
 
-# Fitting K-Means to the dataset
-kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
-y_kmeans = kmeans.fit_predict(X)
+dataset = pd.read_csv('Mall_Customers.csv').as_matrix()
+X = dataset[:, -2:].astype(int)
 
-# Visualising the clusters
-plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
-plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
-plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
-plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
-plt.title('Clusters of customers')
-plt.xlabel('Annual Income (k$)')
-plt.ylabel('Spending Score (1-100)')
-plt.legend()
+# elbow method
+# wcss = []
+# inspected_cluster_numbers = range(1, 11)
+# for i in inspected_cluster_numbers:
+#     model = KMeans(n_clusters=i, random_state=0)
+#     model.fit(X)
+#     score = model.inertia_
+#     wcss.append(score)
+#
+# plt.plot(inspected_cluster_numbers, wcss)
+# plt.xlabel('Number of clusters')
+# plt.ylabel('WCSS')
+# plt.title('Elbow method')
+# plt.show()
+
+optimal_number_of_clusters = 5
+model = KMeans(n_clusters=optimal_number_of_clusters, random_state=0)
+model.fit(X)
+clusters = model.predict(X).reshape(-1, 1)
+cluster_numbers_with_initial_data = np.append(dataset, clusters, axis=1)
+
+# agrrh, use get_named_colors_mapping instead
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
+for row in cluster_numbers_with_initial_data:
+    cluster_number = row[-1]
+    color = colors[cluster_number]
+    year_income = row[-3]
+    spending_score = row[-2]
+    plt.scatter(year_income, spending_score, color=color)
+
 plt.show()
