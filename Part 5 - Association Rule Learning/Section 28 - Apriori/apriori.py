bc0915723c02a4f5cfcf05a7d329741a4907d4be
Kirill Krolik <2902785+avant1@users.noreply.github.com>
2018-03-30 18:26:56 +0300

    apriori using python (well, kind of)

--- Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
@@ -1,19 +1,31 @@
-# Apriori
-
-# Importing the libraries
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-
-# Data Preprocessing
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
-transactions = []
-for i in range(0, 7501):
-    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
-
-# Training Apriori on the dataset
 from apyori import apriori
-rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 
-# Visualising the results
-results = list(rules)
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None).as_matrix()
+
+transactions = []
+for items_in_transaction in dataset:
+    transaction = []
+    for item in items_in_transaction:
+        item = str(item)
+        #np.isnan cannot be used here cause not empty values are strings
+        if item == 'nan':
+            continue
+        transaction.append(item)
+    transactions.append(transaction)
+
+# products bought at least 3 times a week
+min_support = 3 * 7 / len(dataset)
+min_confidence = 0.4
+min_lift = 3
+rules = apriori(
+    transactions,
+    min_support=min_support,
+    min_confidence=min_confidence,
+    min_lift=min_lift,
+    min_length=3
+)
+
+rules = list(rules)
