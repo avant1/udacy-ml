ef4aeb4ec04e1a2caefe6ffff10608cbbe1b31ae
Kirill Krolik <2902785+avant1@users.noreply.github.com>
2019-01-26 01:50:24 +0300

    switch to keras 2 API, still deprecations in OneHotEncoder usage
    No idea how to make OneHotEncoder(categories=...) work.

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/my_ann.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/my_ann.py
@@ -2,32 +2,29 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix, classification_report
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, BatchNormalization, Activation
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 np.random.seed(0)
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+categorical_columns = [1, 2]
+for column in categorical_columns:
+    labelencoder = LabelEncoder()
+    X[:, column] = labelencoder.fit_transform(X[:, column])
 
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features=[1])
-X = onehotencoder.fit_transform(X).toarray()
+onehotencoder = OneHotEncoder(categorical_features=[1], sparse=False)
+X = onehotencoder.fit_transform(X)
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-from sklearn.preprocessing import StandardScaler
 
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
@@ -36,19 +33,17 @@ X_test = sc.transform(X_test)
 input_size = len(X[0])
 classifier = Sequential()
 
-# classifier.add(BatchNormalization(input_dim=input_size, output_dim=input_size))
-
 # two hidden layers
-classifier.add(Dense(input_dim=input_size, output_dim=6, init='uniform', activation='relu'))
-classifier.add(Dense(output_dim=6, init='uniform', activation='relu'))
+classifier.add(Dense(input_dim=input_size, units=6, kernel_initializer='uniform', activation='relu'))
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
 
 # output layer
-# use output_dim=N_classes and 'softmax' activation for multi-class classification
-classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+# use units=N_classes and 'softmax' activation for multi-class classification
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
 
 # use loss='categorical_crossentropy' for multi-class classification
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-classifier.fit(X_train, y_train, batch_size=10, nb_epoch=5)
+classifier.fit(X_train, y_train, batch_size=10, epochs=5)
 
 y_pred_probabilities = classifier.predict(X_test, verbose=1)
 
