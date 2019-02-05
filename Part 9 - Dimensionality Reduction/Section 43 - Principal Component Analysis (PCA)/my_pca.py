c410ca4f13523101e47baf9df576300e5ac1fd9a
Kirill Krolik <2902785+avant1@users.noreply.github.com>
2019-02-05 23:11:02 +0300

    PCA is AWESOME

---
+++ Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/my_pca.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Wine.csv')
+
+# ignoring user ID and gender columns
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+
+explained_variance = pca.explained_variance_ratio_
+print('"Lost" variance: {:.3f}'.format(1 - sum(explained_variance)))
+
+# Fitting the Regression Model to the dataset
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(C=1, random_state=0, solver='lbfgs')
+classifier.fit(X_train, y_train)
+
+score = classifier.score(X_test, y_test)
+print('Score: {:.2f}'.format(score))
+
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+colors = ListedColormap(('red', 'green', 'blue'))
+wtf = classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape)
+plt.contourf(X1, X2, wtf, alpha=0.75, cmap=colors)
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=colors(i), label=j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Component 1')
+plt.ylabel('Component 2')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=colors)
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=colors(i), label=j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Component 1')
+plt.ylabel('Component 2')
+plt.legend()
+plt.show()
